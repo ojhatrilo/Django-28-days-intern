5a934b95bd4a63cca7f948318a9619b2cfcb1060
Ojhatrilo <ojhatrilochan5@gmail.com>
2024-05-22 10:08:03 +0530

    upload

--- Django intenship 28/backend/base/models.py
+++ Django intenship 28/backend/base/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
+
 
 
 class Question(models.Model):
--- Django intenship 28/backend/base/views.py
+++ Django intenship 28/backend/base/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question,Choice
+
 
 
 def index(request):
-    return render(request, 'index.html')
+    data = Question.objects.all()
+    ans = Choice.objects.all()
+    mylist = zip(data,ans)
+    context = {
+            'mylist': mylist,
+        }
+    
+    return render(request, 'index.html', context)
