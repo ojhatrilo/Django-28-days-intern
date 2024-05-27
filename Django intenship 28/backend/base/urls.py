f16d5f1c6838e16e627b6353ae8f9b898b9bd3b3
Ojhatrilo <ojhatrilochan5@gmail.com>
2024-05-27 14:11:55 +0530

    update

--- Django intenship 28/backend/base/urls.py
+++ Django intenship 28/backend/base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="hello"),
-    path('time/', views.date, name="date")
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('update/<int:question_id>/', views.update, name='update'),
+    path('delete/<int:question_id>/', views.delete, name='delete'),
 ]
--- Django intenship 28/backend/base/views.py
+++ Django intenship 28/backend/base/views.py
@@ -1,38 +1,62 @@
-from django.shortcuts import render,redirect
-
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .models import Question,Choice
+from .models import Question, Choice
 
-
-
+# List Questions and Choices
 def index(request):
-    data = Question.objects.all()
-    ans = Choice.objects.all()
-    mylist = zip(data,ans)
+    questions = Question.objects.all()
+    choices = Choice.objects.all()
+    mylist = zip(questions, choices)
     context = {
-            'mylist': mylist,
-        }
-    
+        'mylist': mylist,
+    }
     return render(request, 'index.html', context)
 
-def date(request):
-    if request.method =="POST":
-        question = request.POST['question_text']
-        date = request.POST['pub_date']
-        ans = request.POST['questions']
-        choice = request.POST['choice_text']
+# Create new Question and Choice
+def create(request):
+    if request.method == "POST":
+        question_text = request.POST['question_text']
+        pub_date = request.POST['pub_date']
+        choice_text = request.POST['choice_text']
         votes = request.POST['votes']
 
-        data = Question(question_text=question, pub_date = date)
-        data.save()
-        ans = Choice(question=ans,choice_text=choice,votes=votes)
-        ans.save()
-        return redirect('hello')
-    data = Question.objects.all()
+        question = Question.objects.create(question_text=question_text, pub_date=pub_date)
+        question.save()
+        choice = Choice.objects.create(question=question, choice_text=choice_text, votes=votes)
+        choice.save()
+        return redirect('index')
+    return render(request, 'create.html')
 
-    return render(request, 'time.html',{"question":data})
+# Update existing Question and Choice
+def update(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    choices = Choice.objects.filter(question=question)
+    if request.method == "POST":
+        question.question_text = request.POST['question_text']
+        question.pub_date = request.POST['pub_date']
+        question.save()
 
+        choice_texts = request.POST.getlist('choice_text')
+        votes = request.POST.getlist('votes')
+        for choice, choice_text, vote in zip(choices, choice_texts, votes):
+            choice.choice_text = choice_text
+            choice.votes = vote
+            choice.save()
+        return redirect('index')
 
-    # data = Question.objects.get(id=id)
-    # return render(request, 'time.html',{"data":data})
+    context = {
+        'question': question,
+        'choices': choices,
+    }
+    return render(request, 'update.html', context)
+
+# Delete Question and related Choices
+def delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        question.delete()
+        return redirect('index')
+    context = {
+        'question': question,
+    }
+    return render(request, 'delete.html', context)
